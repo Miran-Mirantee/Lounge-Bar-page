4cd40b3dd1cdc9c41ac05c39b1a8aca6eeef50c3
Miran-Mirantee <killerm11805@hotmail.com>
2022-10-07 01:52:27 +0700

    Added favicon into webpack

--- webpack.config.js
+++ webpack.config.js
@@ -12,6 +12,7 @@ module.exports = {
     plugins: [
         new HtmlWebpackPlugin({
           template: "./src/index.html",
+          favicon: "./src/assets/icon/Piano_Woman.ico",
         }),
     ],
     output: {
